0c7def5c1e7eb852ebd3019ccc4e8404bbe38b9b
cynoteck <cynoteck@cynodoon.com>
2017-09-04 12:26:09 +0530

    material design admin theme setup

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,32 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
 import { AppComponent } from './app.component';
+
 import { RegistrationComponent } from './components/registration/registration.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { SuperAdminComponent } from './components/super-admin/super-admin.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { appRouting } from "app/app.routing";
 import { LoginComponent } from './components/login/login.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    RegistrationComponent,
-    HeaderComponent,
-    FooterComponent,
-    AdminComponent,
-    SuperAdminComponent,
-    SideBarComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    appRouting
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '',redirectTo: 'registration',pathMatch: 'full'},
+    { path: 'registration', component: RegistrationComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: '**', pathMatch:'full', redirectTo: 'registration' }
+];
+
+export const appRouting = RouterModule.forRoot(routes);
